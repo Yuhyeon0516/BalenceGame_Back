33b1a1cb0cc32b8a22b601fcbca6e7fcc13eb378
Yuhyeon0516 <rladbgus0516@naver.com>
2024-01-16 15:28:08 +0900

    Set database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/database.config';
+import { AuthModule } from './auth/auth.module';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env`,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useClass: TypeOrmConfigService,
+        }),
+        AuthModule,
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private configService: ConfigService) {}
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(cookieParser()).forRoutes('*');
+
+        consumer
+            .apply(
+                session({
+                    secret: this.configService.get<string>('COOKIE_SECRET'),
+                    resave: false,
+                    saveUninitialized: false,
+                }),
+            )
+            .forRoutes('*');
+    }
+}
