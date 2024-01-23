ce9b24a9d234d622a9df261e6fa9c7f86e6d2700
Yuhyeon0516 <rladbgus0516@naver.com>
2024-01-23 13:17:08 +0900

    Added origin path

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: ["http://localhost:3000"],
+        origin: [
+            "http://localhost:3000",
+            "https://jh0152park.github.io/BalanceGame_Front",
+        ],
         credentials: true,
         exposedHeaders: ["Authorization"],
     });
